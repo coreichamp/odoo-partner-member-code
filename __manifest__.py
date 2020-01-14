33c6fbead29f646d4d57b7477fc97cf2df87c4ab
Champ <kkw.phattarapon@gmail.com>
2020-01-14 14:58:21 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Partner Member Code",
+    'description': 'Add Member Code on partner',
+    "category": "Extra Tools",
+    "summary": "Add Member Code on partner",
+    'version': '12.0.1.0.0',
+    "author": "CHAMP",
+    "depends": ["base"],
+    "data": ["views/res_partner.xml"],
+    "installable": True,
+    'application': False,
+}
